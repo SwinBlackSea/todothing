48893cd0f253434c74d1956527857c297a6ff95f
SwinBlackSea <>
2020-01-16 14:27:56 +0800

    int8Array

---
+++ int8Array.js
@@ -0,0 +1,10 @@
+/**
+ * The Int8Array typed array represents an array of twos-complement 8-bit signed integers. 
+ * The contents are initialized to 0. Once established, you can reference elements in the array using the object's methods, 
+ * or using standard array index syntax (that is, using bracket notation).
+ */
+
+ /**
+  * Int8Array just include integer which is twos-complement 8-bit signed:只能存储二进制8bit的整数
+  * capacity is initialized and cannot change after the array is inited:容量在初始化之后不能发生更改
+  */
